08d9c0a2d68dbe12106954bdfb42a1a4a37dc9f0
Tringa <95575981+TringaI@users.noreply.github.com>
2025-04-12 22:01:12 +0200

    client and three js error

---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Scene from './Scene'
+function Home() {
+  return (
+    <div>
+        <Scene/>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login'
+import Signup from './Pages/Signup/Signup'
+import Products from './Pages/Products/Products'
+import Profile from './Pages/Profile/Profile'
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/login' element={<Login/>}/>
+        <Route exact path='/signup' element={<Signup/>}/>
+        <Route exact path='/signup' element={<Signup/>}/>
+        <Route exact path='/profile' element={<Profile/>}/>
+        <Route exact path='/products' element={<Products/>}/>
+    </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
