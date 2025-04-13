991e3db4d223845a86360a6af973cbca71e441e5
Tringa <95575981+TringaI@users.noreply.github.com>
2025-04-13 08:46:49 +0200

    new changes-no conflicts

--- client/src/Pages/Products/ProductSpecific.jsx
+++ client/src/Pages/Products/ProductSpecific.jsx
@@ -191,7 +191,6 @@ function ProductSpecific() {
 
   return (
     <div className='w-full mt-40 flex items-center justify-center'>
-<<<<<<< Updated upstream
       <div className='w-[80vw] grid gap-5 grid-cols-12'>
         <div className="col-span-6">
           <img 
@@ -201,25 +200,6 @@ function ProductSpecific() {
               : "/images/product-images/product-1.png"} 
             alt={product.name} 
           />
-=======
-        <div className='w-[80vw] grid gap-5 grid-cols-12'>
-            <div className="col-span-6">
-              <img className='rounded-md' src="/images/product-images/product-1.png" alt="" />
-            </div>
-            <div className="col-span-6 flex flex-col border-l-[#478e69] border-l-2 pl-5">
-                <h1 className='moret text-5xl'>Domate Bio</h1>
-                <p className='poppins text-gray-500 text-xl'>Fermer: Filan Fisteku</p>
-                <p className='poppins text-gray-600 text-base mt-10'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum maiores ipsa maxime porro quis neque quo quaerat magni repellat, 
-                  sapiente unde natus quibusdam fugiat, recusandae impedit! Reiciendis dignissimos cupiditate magnam?
-                </p>
-                <p className='poppins text-gray-600 text-base mt-5'>
-                  Cmimi: <span className='font-medium'>40$</span>
-                </p>
-                <a href='/360' className="underline-wavy-green poppins mt-5 text-xl font-medium">Shikoni produktin ne 360</a>
-                <button className='mt-5 light-green-bg text-xl poppins w-fit py-2 px-4'>Beni kerkese per blejre</button>
-            </div>
->>>>>>> Stashed changes
         </div>
         <div className="col-span-6 flex flex-col border-l-[#478e69] border-l-2 pl-5">
           <h1 className='moret text-5xl'>{product.name}</h1>
